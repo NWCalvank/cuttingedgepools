3d97656e8cc000d6ec1615b8327fb74698762067
NWCalvank <nwcalvank@gmail.com>
2016-09-09 16:22:55 -0400

    first full commit

---
+++ plugin/app.js
@@ -0,0 +1,9 @@
+//JavaScript Document
+
+// document.getElementById("boxOne").addEventListener("mouseover", function() {
+//   document.getElementById("boxes").style.backgroundImage = "url(images/image1.jpeg)";
+// }, false);
+
+// document.getElementById("boxOne").addEventListener("mouseout", function() {
+//    document.getElementById("boxes").style.backgroundImage = "";
+// }, false);
