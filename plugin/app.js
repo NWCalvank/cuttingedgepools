d0df7e67b4044b5f301af647e2395299d852fa5f
NWCalvank <nwcalvank@gmail.com>
2016-10-10 14:39:47 -0400

    Modular restructure and image file size improvements

--- plugin/app.js
+++
@@ -1,9 +0,0 @@
-//JavaScript Document
-
-// document.getElementById("boxOne").addEventListener("mouseover", function() {
-//   document.getElementById("boxes").style.backgroundImage = "url(images/image1.jpeg)";
-// }, false);
-
-// document.getElementById("boxOne").addEventListener("mouseout", function() {
-//    document.getElementById("boxes").style.backgroundImage = "";
-// }, false);
