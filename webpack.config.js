f254799714c6e7319bff8ee82688552a3044298b
NWCalvank <nwcalvank@gmail.com>
2018-04-15 15:55:13 -0700

    Minify JS with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
-var { resolve } = require('path');
+const { resolve } = require('path');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
 module.exports = {
-	context: resolve('src/js'),
-	entry: './app.js',
-	output: {
-		path: resolve('build'),
-		filename: 'bundle.js',
-		publicPath: '/build/',
-	},
-	module: {
-		loaders: [
-		{test: /\.js$/, loaders: [], exclude: /node_modules/},
-		{test: /\.css$/, loaders: ['style-loader', 'css-loader']},
-		],
-	}
-}
+  context: resolve('src/js'),
+  entry: './app.js',
+  output: {
+    path: resolve('build'),
+    filename: 'bundle.js',
+    publicPath: '/build/',
+  },
+  plugins: [new MinifyPlugin({}, {})],
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: [], exclude: /node_modules/ },
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+    ],
+  },
+};
